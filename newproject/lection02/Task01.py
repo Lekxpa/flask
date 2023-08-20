77342b25c947a2dd6f3ddaee73d4c6b07d6f35a4
Lekxpa <LekxpaP@gmail.com>
2023-08-20 23:47:19 +0300

    add file in folder HW_01

--- newproject/lection02/Task01.py
+++ newproject/lection02/Task01.py
@@ -1,11 +1,11 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hi!'
+    return 'Hi-ffi!'
 
 
 @app.route('/test_url_for/<int:num>/')
@@ -16,6 +16,18 @@ def test_url(num):
     text += f'Функция {url_for("test_url", num=42, data="new_data", pi=3.14515) = }<br>'
     return text
 
+@app.route('/sum/<int:num1>-<int:num2>/')
+def sum_num(num1, num2):
+    res = num1 + num2
+    return f"{num1} + {num2} = {res}"
+
+@app.route('/about/')
+def about():
+    context = {
+        'title': "Обо мне",
+        'name': "Helen",
+    }
+    return render_template('about.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
